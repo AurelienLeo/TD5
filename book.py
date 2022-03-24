f9cebc720edc6a1d23276c18a48da92f9f3b3f40
AurelienLeo <aurelienleonard12@gmail.com>
2022-03-24 17:51:42 +0000

    exo3

--- book.py
+++ book.py
@@ -1,6 +1,14 @@
 class Book:
     def __init__(self,name):
         self.name=name
+        self.list = list()
+
+    def insert_buy(self,qtty,price):
+        self.list.append(Order(true,qtty,price,len(self.list))
+    
+    def insert_sell(self,qtty,price):
+        self.list.append(Order(false,qtty,price,len(self.list))
+
 
 class Order:
     def __init__(self,buy,qtty,price,id):
@@ -8,7 +16,13 @@ class Order:
         self.qtty=qtty
         self.price=price
         self.id=id
+    
+    def isBuy(self):
+        msg = "Sell"
+        if self.buy:
+            msg = "Buy"
+        return msg
 
-
+    def __str__
 
 
