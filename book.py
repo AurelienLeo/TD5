e109c5ab938c62546eb2c214dea3ce2e9d6e16f2
heloise MALCLES <heloise.malcles@yahoo.fr>
2022-03-24 17:39:51 +0000

    h

--- book.py
+++ book.py
@@ -1,5 +1,14 @@
 class Book:
+    def __init__(self,name):
+        self.name=name
 
 class Order:
+    def __init__(self,buy,qtty,price,id):
+        self.buy=buy
+        self.qtty=qtty
+        self.price=price
+        self.id=id
+
+
 
 
