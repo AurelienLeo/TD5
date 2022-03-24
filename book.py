29837cde056e3f79b119804a91577a6231c9695c
AurelienLeo <aurelienleonard12@gmail.com>
2022-03-24 18:44:13 +0000

    exo3

--- book.py
+++ book.py
@@ -1,14 +1,25 @@
 class Book:
     def __init__(self,name):
         self.name=name
-        self.list = list()
+        self.list_buy = list()
+        self.list_sell = list()
+        self.count_order=0
 
     def insert_buy(self,qtty,price):
-        self.list.append(Order(true,qtty,price,len(self.list))
-    
-    def insert_sell(self,qtty,price):
-        self.list.append(Order(false,qtty,price,len(self.list))
+        self.count_order += 1
+        self.list.append(Order(true,qtty,price,self.count_order))
+        print(Order(true,qtty,price,self.count_order))
+        self.list_buy=sorted(self.list_buy,key=lambda x:x.price)
+        #self.list_buy=sorted(sorted(self.list_buy, key=lambda x:x.id),key=lambda x:x.price)
 
+        for i in self.list_buy:
+            print(i)
+   
+            
+    def insert_sell(self,qtty,price):
+        self.count_order += 1
+        self.list_sell.append(Order(false,qtty,price,self.count_order))
+        self.list_sell = sorted(sorted(self.list_sell, key=lambda x:x.id),key=lambda x:x.price)
 
 class Order:
     def __init__(self,buy,qtty,price,id):
@@ -23,6 +34,8 @@ class Order:
             msg = "Buy"
         return msg
 
-    def __str__
-
+    def __str__(self):
+       type = self.isBuy
+       msg = type + " " + str(self.qtty) +"@"+ str(self.price) + " id=" + str(self.id)
+       return msg
 
